0f3c920999bc97fd3b2aefb664747f8a205a3cf2
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2022-10-19 19:44:16 +0100

    Updates

---
+++ precache-manifest.dee08f3c0b7e4af93b6118894fe9575a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4bc28e2fe8f2876e333",
+    "url": "/random-quotes-generator/static/css/main.566c7c4b.chunk.css"
+  },
+  {
+    "revision": "a4bc28e2fe8f2876e333",
+    "url": "/random-quotes-generator/static/js/main.a4bc28e2.chunk.js"
+  },
+  {
+    "revision": "64c0f203beb83e3b6b53",
+    "url": "/random-quotes-generator/static/js/1.64c0f203.chunk.js"
+  },
+  {
+    "revision": "511860ec2ba9533208e7",
+    "url": "/random-quotes-generator/static/js/runtime~main.511860ec.js"
+  },
+  {
+    "revision": "4963995ac51b4853f362e98b5a453244",
+    "url": "/random-quotes-generator/index.html"
+  }
+];
